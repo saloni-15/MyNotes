d11f8c56f65955a27876b198c635e8eb58369c3f
Saloni Subedi <subedisaloni2002@gmail.com>
2021-02-02 13:23:23 +0530

    add map function

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,22 @@
 import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
+import notes from "../notes";
 import Note from "./Note";
+
+function createNote(note) {
+  return (
+   
+      <Note key={note.key} title={note.title} content={note.content} />
+   
+  );
+}
 
 function App() {
   return (
     <div>
       <Header></Header>
-      <Note></Note>
+      {notes.map(createNote)}
       <Footer></Footer>
     </div>
   );
